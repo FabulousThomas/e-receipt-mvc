8153f716f4535b4caf6805ede68be177859cc4ce
Mba Thomas <angelfab631@gmail.com>
2022-07-20 04:09:00 +0100

    Create register.php
    Register page :: Contains Registration form

---
+++ app/views/users/register.php
@@ -0,0 +1,60 @@
+<?php
+
+// session_start();
+// require_once "./functions/connection.php";
+// require_once "./functions/function.php";
+// require_once "./functions/login.php";
+// include "./functions/timeout.php";
+
+?>
+
+<?php require APPROOT . '/views/inc/header.php' ?>
+
+<body>
+   <div class="container">
+
+   <div class="row">
+      <div class="col-lg-5 col-md-12 mx-auto p-0">
+         <div class="card card-body bg-light p-2 mt-5">
+         <form action="" method="post" class="form py-4" id="form" enctype="multipart/form-data">
+         <div class="bank-logo pt-0 pb-3">
+            <img src="<?php echo URLROOT; ?>/img/viewdeep-logo.png" width="130px" height="130px">
+            <h5>VIEWDEEP E-RECEIPT GENERATOR</h5>
+         </div>
+         <div class="form-group">
+            <input type="text" name="username" class="form-control form-control-lg <?php echo (empty($data['username'])) ? 'is-invalid' : ''; ?>" placeholder="USERNAME" value="<?php echo $data['username']; ?>" spellcheck="false" autocomplete="off" maxlength="50">
+            <small class="invalid-feedback"><?php echo $data['username_err']; ?></small>
+         </div>
+         <div class="form-group">
+            <input type="email" name="email" class="form-control form-control-lg <?php echo (empty($data['email'])) ? 'is-invalid' : ''; ?>" placeholder="EMAIL" value="<?php echo $data['email']; ?>" spellcheck="false" autocomplete="off">
+            <small class="invalid-feedback"><?php echo $data['email_err']; ?></small>
+         </div>
+         <div class="form-group">
+            <input type="password" name="password" class="form-control form-control-lg <?php echo (empty($data['password'])) ? 'is-invalid' : ''; ?>" placeholder="PASSWORD" value="<?php echo $data['password']; ?>" maxlength="50">
+            <small class="invalid-feedback"><?php echo $data['password_err']; ?></small>
+         </div>
+         <div class="row">
+            <div class="col">
+            <button type="submit" name="login_btn" class="btn btn-primary form-control-lg rounded-0 btn-block" id="submit">Register</button>
+            </div>
+            <div class="col">
+           <a href="<?php echo URLROOT;?>/users/login" class="btn btn-light form-control-lg rounded-0" style="font-size: .9rem ;">Have an account? Login</a>
+            </div>
+         </div>
+         <!-- <div class="form-group">
+            <?php if ($loginErr) { ?>
+               <div style="color: red;"><?php echo $loginErr; ?></div>
+            <?php } ?>
+         </div> -->
+      </form>
+         </div>
+         
+      </div>
+   </div>
+      
+   </div>
+
+   <?php require APPROOT . '/views/inc/footer.php' ?>
+</body>
+
+</html>
