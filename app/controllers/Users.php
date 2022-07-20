61f657c1526dc4baa0da750302366e51318a227f
Mba Thomas <angelfab631@gmail.com>
2022-07-20 04:08:11 +0100

    Update Users.php

--- app/controllers/Users.php
+++ app/controllers/Users.php
@@ -22,16 +22,16 @@ class Users extends Controller
             'password_err' => '',
          ];
          // Validate Username
-         if(empty($data['username'])) {
+         if (empty($data['username'])) {
             $data['username_err'] = 'Please enter username';
          }
          // Validate password
-         if(empty($data['password'])) {
+         if (empty($data['password'])) {
             $data['password_err'] = 'Please enter password';
          }
 
          // Make sure errors are empty
-         if(empty($data['username_err']) && empty($data['password_err'])) {
+         if (empty($data['username_err']) && empty($data['password_err'])) {
             // Validate (passed)
             die('SUCCESS');
          } else {
@@ -54,9 +54,53 @@ class Users extends Controller
    // Load Register view
    public function register()
    {
-      $data = [
-         'title' => 'VIEWDEEP REGISTER PAGE'
-      ];
-      $this->view('users/register', $data);
+      // Check for Request
+      if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+         // Process form
+         // Sanitize form inputs
+         $_POST = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+         // Init data
+         $data = [
+            'email' => trim($_POST['email']),
+            'username' => trim($_POST['username']),
+            'password' => trim($_POST['password']),
+            'email_err' => '',
+            'username_err' => '',
+            'password_err' => '',
+         ];
+         // Validate Username
+         if (empty($data['username'])) {
+            $data['username_err'] = 'Please enter username';
+         }
+         // Validate Email
+         if (empty($data['email'])) {
+            $data['email_err'] = 'Please enter email';
+         }
+         // Validate password
+         if (empty($data['password'])) {
+            $data['password_err'] = 'Please enter password';
+         }
+
+         // Make sure errors are empty
+         if (empty($data['username_err']) && empty($data['password_err']) && empty($data['email_err'])) {
+            // Validate (passed)
+            die('SUCCESS');
+         } else {
+            // Load views with errors
+            $this->view('users/register', $data);
+         }
+      } else {
+         // Init data
+         $data = [
+            'email' => '',
+            'username' => '',
+            'password' => '',
+            'email_err' => '',
+            'username_err' => '',
+            'password_err' => '',
+         ];
+         // Load View
+         $this->view('users/register', $data);
+      }
    }
 }
