5eb154b635ca2f67dbb028bfbe82aef0d534310c
Mba Thomas <angelfab631@gmail.com>
2022-07-22 03:14:22 +0100

    Create helpers.php

---
+++ app/helpers/helpers.php
@@ -0,0 +1,20 @@
+<?php 
+  // RANDOM NUMBER FUNCTION
+ function random_num($length)
+  {
+     $text = "";
+     if ($length < 5) {
+        $length = 5;
+     }
+     $len = rand(4, $length);
+
+     for ($i = 0; $i < $len; $i++) {
+        $text .= rand(0, 9);
+     }
+     return $text;
+  } 
+
+  // Url redirect
+  function redirect($pages) {
+    header('Location: ' . URLROOT . '/' .$pages);
+  }
