1ed36530d35832f3c1f44cf5c86a55a63efba8e6
Mba Thomas <angelfab631@gmail.com>
2022-07-25 13:01:17 +0100

    Create sidebar.php

---
+++ app/views/inc/sidebar.php
@@ -0,0 +1,35 @@
+<input type="checkbox" name="" id="nav-toggle">
+<div class="sidebar">
+   <div class="sidebar-brand">
+      <h2><span class="lab la-accusoft"></span> <span>E-Receipt</span></h2>
+   </div>
+   <div class="sidebar-menu">
+      <ul>
+         <li>
+            <a href="<?php echo URLROOT; ?>/pages/index" class="active"><span class="las la-igloo"></span>
+               <span>Dashboard</span></a>
+         </li>
+         <!-- <li>
+            <a href="./form.php"><span class="las la-receipt"></span>
+               <span>New Receipt</span></a>
+         </li> -->
+         <li>
+            <a href="<?php echo URLROOT; ?>/pages/invoice"><span class="las la-clipboard-list"></span>
+               <span>Invoice</span></a>
+         </li>
+         <li>
+            <a href="<?php echo URLROOT; ?>/pages/profile"><span class="las la-user"></span>
+               <span>Customer Profile</span></a>
+         </li>
+         <li>
+            <a href="<?php echo URLROOT; ?>/pages/sessions"><span class="las la-users"></span>
+               <span>Login Sessions</span></a>
+         </li>
+         <li>
+            <a href="<?php echo URLROOT; ?>/pages/sharing"><span class="las la-percentage"></span>
+               <span>Sharing</span></a>
+         </li>
+
+      </ul>
+   </div>
+</div>
