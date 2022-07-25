bbcd9c7cee8e776abb6b7dbc60b47698ccda3661
Mba Thomas <angelfab631@gmail.com>
2022-07-25 14:42:36 +0100

    Update header.php

--- app/views/inc/header.php
+++ app/views/inc/header.php
@@ -9,9 +9,9 @@
 
    <!-- Bootstrap CSS -->
    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/font-awesome-line-awesome/css/all.min.css">
+   <link rel="stylesheet" href="https://maxst.icons8.com/vue-static/landings/line-awesome/line-awesome/1.3.0/css/line-awesome.min.css">
    <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/css.css">
-   <link rel="stylesheet" href="<?php echo APPROOT; ?>/assets/css/dashboard.css">
+   <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/dashboard.css">
    <title><?php echo SITENAME ?></title>
 </head>
 
