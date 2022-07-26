19b96ad25d33b96401bbe9a562e1489e3b87f2aa
Mba Thomas <angelfab631@gmail.com>
2022-07-26 05:57:11 +0100

    Create session_helper.php

---
+++ app/helpers/session_helper.php
@@ -0,0 +1,34 @@
+<?php 
+
+// Message function
+session_start();
+function flashMsg($name = '', $message = '', $class = 'alert alert-success')
+{
+   if (!empty($name)) {
+      if (!empty($message) && empty($_SESSION[$name])) {
+         if (!empty($_SESSION[$name])) {
+            unset($_SESSION[$name]);
+         }
+
+         if (!empty($_SESSION[$name . '_class'])) {
+            unset($_SESSION[$name . '_class']);
+         }
+
+         $_SESSION[$name] = $message;
+         $_SESSION[$name . '_class'] = $class;
+      } elseif (empty($message) && !empty($_SESSION[$name])) {
+         $class = !empty($_SESSION[$name . '_class']) ? $_SESSION[$name . '_class'] : '';
+         echo '<div class="text-center ' . $class . '" id="msg-flash">' . $_SESSION[$name] . '</div>';
+         unset($_SESSION[$name]);
+         unset($_SESSION[$name . '_class']);
+      }
+   }
+}
+   // Checks if user is logged in
+ function isLoggedIn() {
+      if(isset($_SESSION['user_id'])) {
+         return true;
+      } else {
+         return false;
+      }
+   }
