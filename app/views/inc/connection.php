e7d0cd9f95e35241a8c3e55b920f51c697b33a9c
Mba Thomas <angelfab631@gmail.com>
2022-08-28 10:55:21 +0100

    Update connection.php

--- app/views/inc/connection.php
+++ app/views/inc/connection.php
@@ -1,7 +1,7 @@
-<?php 
+<?php
 
 $conn = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
-if(!$conn) {
+if (!$conn) {
    die('CONNECTED');
 }
 
@@ -16,3 +16,29 @@ $invoice = mysqli_query($conn, $query);
 
 $query = "SELECT count(user_id) AS counts FROM users";
 $users = mysqli_query($conn, $query);
+
+if (isset($_POST['btnUpdateReceipt'])) {
+
+   // Bind values
+   $id = $_POST['userId'];
+   $date = $_POST['date'];
+   $estate = $_POST['estate'];
+   $received_from = $_POST['received_from'];
+   $sum_of = $_POST['sum_of'];
+   $payment_figure = $_POST['payment_figure'];
+   $payment_mode = $_POST['payment_mode'];
+   $payment_for = $_POST['payment_for'];
+   $no_unit = $_POST['no_unit'];
+   $amount_paid = $_POST['amount_paid'];
+   $outstanding = $_POST['outstanding'];
+   $balance = $_POST['balance'];
+
+   $sql = "UPDATE e_receipt SET date = '$date', estate = '$estate', received_from = '$received_from', sum_of = '$sum_of', payment_mode = '$payment_mode', payment_for = '$payment_for', payment_figure = '$payment_figure', no_unit = '$no_unit', amount_paid = '$amount_paid', outstanding = '$outstanding', balance = '$balance' WHERE user_id = '$id'";
+
+   if(mysqli_query($conn, $sql)) {
+      flashMsg('msg', 'Receipt updated');
+      redirect('pages/index');
+   } else {
+      die('Unable to update' . mysqli_error($conn));
+   }
+}
