6a24f64e9c72dd5c17d94906bb648ec84fcaf3d4
Mba Thomas <angelfab631@gmail.com>
2022-08-31 02:22:59 +0100

    Create currency.js

---
+++ public/js/currency.js
@@ -0,0 +1,36 @@
+// CURRENCY FORMAT FUNCTIONS
+
+var currencyInput = document.querySelectorAll('input[type="currency"]')
+var currency = 'GBP' // https://www.currency-iso.org/dam/downloads/lists/list_one.xml
+
+// format inital value
+onblur({ target: currencyInput })
+
+// bind event listeners
+currencyInput.addEventListener('focus', onFocus)
+currencyInput.addEventListener('blur', onblur)
+
+
+function localStringToNumber(s) {
+    return Number(String(s).replace(/[^0-9.-]+/g, ""))
+}
+
+function onblur(e) {
+    var value = e.target.value;
+    e.target.value = value ? localStringToNumber(value) : ''
+}
+
+function onFocus(e) {
+    var value = e.target.value
+
+    var options = {
+        maximumFractionDigits: 2,
+        currency: currency,
+        //   style: "currency",
+        currencyDisplay: "symbol"
+    }
+
+    e.target.value = (value || value === 0) ?
+        localStringToNumber(value).toLocaleString(undefined, options) : '';
+
+}
