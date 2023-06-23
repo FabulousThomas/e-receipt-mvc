3fecb079d9522bba5d6b6e205b01271ac62fd17f
Mba Thomas <angelfab631@gmail.com>
2023-06-23 04:52:39 +0200

    Update bootstrap.php

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -3,6 +3,7 @@
 require_once 'config/config.php';
 // Load helpers
 require_once 'helpers/helpers.php';
+require_once 'config/connection.php';
 require_once 'helpers/session_helper.php';
 
 spl_autoload_register(function($className) {
